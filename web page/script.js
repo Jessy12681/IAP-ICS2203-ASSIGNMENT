120829df80134753dae1dc8358560974789a595f
Jessy12681 <125574767+Jessy12681@users.noreply.github.com>
2025-05-06 17:03:03 +0300

    Add files via upload
    web page example

---
+++ web page/script.js
@@ -0,0 +1,6 @@
+function openloginform() {
+    document.getElementById("login").style.display = "block";
+}
+function closeloginform() {
+    document.getElementById("login").style.display = "none";
+}
